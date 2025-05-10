3d759262067a3b0db36b245a7a957d2e19cbdb13
Daniel Boluda <72820621+Daniel-Boluda@users.noreply.github.com>
2025-05-11 01:04:11 +0200

    homeassistant: revert

--- apps/home-assistant/ha-custom-s3/dockerbuild.sh
+++
@@ -1,3 +0,0 @@
-docker build -t homeassistant-minio-patched -f Dockerfile .
-docker tag homeassistant-minio-patched bolferdocker/homeassistant-minio-patched:2025.5.1
-docker push bolferdocker/homeassistant-minio-patched:2025.5.1
